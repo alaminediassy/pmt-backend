079b8579cc0e67036a4cb884e5e7431722ce7837
Alamiine <diassy.alamine@gmail.com>
2024-10-16 00:20:04 +0200

    Fix bugs

--- src/main/java/com/visiplus/pmt/repository/ProjectRepository.java
+++ src/main/java/com/visiplus/pmt/repository/ProjectRepository.java
@@ -1,7 +1,12 @@
 package com.visiplus.pmt.repository;
 
 import com.visiplus.pmt.entity.Project;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Query("SELECT p FROM Project p JOIN p.membersWithRoles m WHERE p.owner.id = :userId OR m.member.id = :userId")
+    List<Project> findProjectsByUserId(Long userId);
 }
--- src/main/java/com/visiplus/pmt/service/ProjectService.java
+++ src/main/java/com/visiplus/pmt/service/ProjectService.java
@@ -4,9 +4,13 @@ import com.visiplus.pmt.entity.Project;
 import com.visiplus.pmt.entity.ProjectMemberRole;
 import com.visiplus.pmt.enums.Role;
 
+import java.util.List;
+
 public interface ProjectService {
     Project createProject(Project project, Long userId);
     Project addMemberToProject(Long projectId, String email);
     ProjectMemberRole assignRoleToMember(Long projectId, Long memberId, Role role);
     Project getProjectById(Long projectId);
+    List<Project> getAllProjects();
+    List<Project> getProjectsByUserId(Long userId);
 }
