3080111ff14a2ab475163eea84163db1b518d531
Yacine DIOP <yacine@Yacines-MacBook-Air.local>
2024-11-03 16:32:21 -0500

    fixing failing tests when clone repo

--- src/main/java/com/visiplus/pmt/PmtApplication.java
+++ src/main/java/com/visiplus/pmt/PmtApplication.java
@@ -2,6 +2,9 @@ package com.visiplus.pmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootApplication
 public class PmtApplication {
@@ -10,4 +13,9 @@ public class PmtApplication {
 		SpringApplication.run(PmtApplication.class, args);
 	}
 
+
+	@Bean
+	public JavaMailSender javaMailSender() {
+		return new JavaMailSenderImpl();
+	}
 }
